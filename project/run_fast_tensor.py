a758ade69c610faeda66499d9ac0293f755c73df
Kelly Deng <kellykq1030@gmail.com>
2024-11-13 16:54:26 -0500

    task3.2_test4

--- project/run_fast_tensor.py
+++ project/run_fast_tensor.py
@@ -10,8 +10,8 @@ if numba.cuda.is_available():
     GPUBackend = minitorch.TensorBackend(minitorch.CudaOps)
 
 
-def default_log_fn(epoch, total_loss, correct, losses):
-    print("Epoch ", epoch, " loss ", total_loss, "correct", correct)
+def default_log_fn(epoch, total_loss, correct, losses, time_taken):
+    print("Epoch ", epoch, " loss ", total_loss, "correct", correct, "time", time_taken)
 
 
 def RParam(*shape, backend):
@@ -44,19 +44,12 @@ class Linear(minitorch.Module):
         self.out_size = out_size
 
     def forward(self, x):
-        """weight matrix mult input then add bias, output a tensor"""
-        # type Parameter .value is the tensor
-        # inputs = list of tensors, each representing a data point initially
-        # return self.weights.value * inputs + self.bias.value
-
-        # initially call fwd on dataset which has shape (num_pts, 2) = (num_pts, in_size) for first layer
-        num_pts, in_size = x.shape
-        # first layer weight has shape (in_size, out_size) = (2, hidden_layers)
-        # want to broadcast so that (1, in_size, out_size) and (num_pts, in_size, 1)
-        matix_mul = (self.weights.value.view(1, in_size, self.out_size) * x.view(num_pts, in_size, 1)).sum(1).view(num_pts, self.out_size)
-        # result is (num_pts, out_size), i.e. one output for each datapoint
-
-        return matix_mul + self.bias.value.view(1, self.out_size)
+        # TODO: Implement for Task 3.5.
+        batch, in_size = x.shape
+        return (
+            self.weights.value.view(1, in_size, self.out_size)
+            * x.view(batch, in_size, 1)
+        ).sum(1).view(batch, self.out_size) + self.bias.value.view(self.out_size)
 
 
 class FastTrain:
@@ -72,11 +65,13 @@ class FastTrain:
         return self.model.forward(minitorch.tensor(X, backend=self.backend))
 
     def train(self, data, learning_rate, max_epochs=500, log_fn=default_log_fn):
+
         self.model = Network(self.hidden_layers, self.backend)
         optim = minitorch.SGD(self.model.parameters(), learning_rate)
         BATCH = 10
         losses = []
-
+        import time
+        time_a = time.time()
         for epoch in range(max_epochs):
             total_loss = 0.0
             c = list(zip(data.X, data.y))
@@ -107,7 +102,9 @@ class FastTrain:
                 out = self.model.forward(X).view(y.shape[0])
                 y2 = minitorch.tensor(data.y)
                 correct = int(((out.detach() > 0.5) == y2).sum()[0])
-                log_fn(epoch, total_loss, correct, losses)
+                time_b = time.time()
+                log_fn(epoch, total_loss, correct, losses, time_b - time_a)
+                time_a = time_b
 
 
 if __name__ == "__main__":
